b469636ce1c316306fd68635a94d6f09d0b41745
John Koerner <github@riddlepop.com>
2017-01-02 21:43:03 -0500

    Initial Version

---
+++ MathLibrary/Addition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace MathLibrary
+{
+    public static class Addition
+    {
+        public static int Add(params int []i)
+        {
+            return i?.Sum() ?? 0;
+        }
+    }
+}
---
+++ UnitTests/AdditionTests.cs
@@ -0,0 +1,31 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLibrary;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class AdditionTests
+    {
+        [TestMethod]
+        public void BasicAdd()
+        {
+            var result = Addition.Add(5, 5);
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void SingleItem()
+        {
+            var result = Addition.Add(4);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void NullTest()
+        {
+            var result = Addition.Add(null);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
